ba0b2656953ebdeae47034bd23f8a2f149c4e921
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-01-15 23:16:13 +0100

    added console .log for content script

--- browserextension/contentScript.js
+++ browserextension/contentScript.js
@@ -1,6 +1,7 @@
 chrome.storage.sync.get("blockedSites", (data) => {
   const currentSite = window.location.hostname;
   const blockedSites = data.blockedSites || [];
+  console.log("Content script loaded");
 
   if (blockedSites.includes(currentSite)) {
     window.location.href = "https://www.google.com";
