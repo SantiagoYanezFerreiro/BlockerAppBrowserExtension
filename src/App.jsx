dce3db201137b37345f9c58e084916baa2e85b7c
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-02-11 22:35:49 +0100

    set react router for other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import WebsiteBlocker from "./components/WebsiteBlocker";
+import Blocks from "./components/Blocks";
+import Stats from "./components/Stats";
+import Settings from "./components/Settings";
 import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("overview");
+
   return (
     <div>
-      <WebsiteBlocker />
+      <nav>
+        <button onClick={() => setCurrentPage("websiteblocker")}>
+          Blocker
+        </button>
+        <button onClick={() => setCurrentPage("blocks")}>Blocks</button>
+        <button onClick={() => setCurrentPage("stats")}>Stats</button>
+        <button onClick={() => setCurrentPage("settings")}>Settings</button>
+      </nav>
+      {/* Show Content */}
+      {currentPage === "websiteblocker" && <WebsiteBlocker />}
+      {currentPage === "blocks" && <Blocks />}
+      {currentPage === "stats" && <Stats />}
+      {currentPage === "settings" && <Settings />}
     </div>
   );
 }
---
+++ src/components/Blocks.jsx
@@ -0,0 +1,3 @@
+export default function Blocks() {
+  return <div>Blocks</div>;
+}
