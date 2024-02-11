30bf02e787f792ebba6d0442494b631c3ad17efb
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-02-11 23:11:43 +0100

    set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import WebsiteBlocker from "./components/WebsiteBlocker";
 import Blocks from "./components/Blocks";
 import Stats from "./components/Stats";
@@ -6,24 +6,24 @@ import Settings from "./components/Settings";
 import "./App.css";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("overview");
-
   return (
-    <div>
-      <nav>
-        <button onClick={() => setCurrentPage("websiteblocker")}>
-          Blocker
-        </button>
-        <button onClick={() => setCurrentPage("blocks")}>Blocks</button>
-        <button onClick={() => setCurrentPage("stats")}>Stats</button>
-        <button onClick={() => setCurrentPage("settings")}>Settings</button>
-      </nav>
-      {/* Show Content */}
-      {currentPage === "websiteblocker" && <WebsiteBlocker />}
-      {currentPage === "blocks" && <Blocks />}
-      {currentPage === "stats" && <Stats />}
-      {currentPage === "settings" && <Settings />}
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">WebsiteBlocker</Link>
+          <Link to="/blocks">Blocks</Link>
+          <Link to="/stats">Stats</Link>
+          <Link to="/settings">Settings</Link>
+        </nav>
+        {/* Show Content */}
+        <Routes>
+          <Route exact path="/" component={WebsiteBlocker} />
+          <Route path="/blocks" component={Blocks} />
+          <Route path="/stats" component={Stats} />
+          <Route path="/settings" component={Settings} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
