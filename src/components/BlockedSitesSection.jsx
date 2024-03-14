e40140c2b3c875dd0aa3dcec302d3ebc9ddf7430
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-03-14 23:54:40 +0100

    fixed select section  logic

--- src/components/BlockedSitesSection.jsx
+++ src/components/BlockedSitesSection.jsx
@@ -94,7 +94,7 @@ export default function BlockedSitesSection({
   };
 
   const renderLockMethodSelector = () => {
-    if (section.locked) {
+    if (!section.locked) {
       return (
         <select
           value={section.lockMethod || ""}
