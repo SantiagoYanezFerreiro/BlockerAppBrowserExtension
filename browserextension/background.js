1e68819ae0fade3c88775d0c9135940066f45362
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-03-29 22:37:37 +0100

    added logic to check for restartlock on background.js

--- browserextension/background.js
+++ browserextension/background.js
@@ -1,3 +1,15 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed/updated");
 });
+
+//Clear restart locks when the browser starts.
+chrome.runtime.onStartup.addListener(() => {
+  chrome.storage.local(null, (items) => {
+    const keysToRemove = Object.keys(items).filter((key) =>
+      key.startsWith("restartLock")
+    );
+    chrome.storage.local.remove(keysToRemove, () => {
+      console.log("all restart locks cleared");
+    });
+  });
+});
