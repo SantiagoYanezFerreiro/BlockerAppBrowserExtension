c09c229eff5632fa0521d2c3067e673075ac4dac
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-04-08 23:58:34 +0200

    created new lockOptions component to separate logic

--- src/components/LockOptions.jsx
+++ src/components/LockOptions.jsx
@@ -1,38 +1,15 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-import "../BlockedSitesSection.css";
-import LockOptions from "../LockOptions";
-import { FaPencilAlt } from "react-icons/fa";
-import { AiOutlineDelete } from "react-icons/ai";
-import { VscDiffAdded } from "react-icons/vsc";
-import { FaRegWindowClose } from "react-icons/fa";
-import { TfiSave } from "react-icons/tfi";
-import { FaLock } from "react-icons/fa";
-import { FaLockOpen } from "react-icons/fa";
 
-export default function BlockedSitesSection({
-  index,
-  title,
+export default function LockOptions({
   section,
-  sites,
-  isModalOpen,
-  onCloseModal,
-  onToggleModal,
-  onAddWebsite,
-  onEditWebsite,
-  onEditSectionTitle,
-  onDeleteSection,
-  onDeleteWebsite,
+  index,
   onLockMethodChange,
   onUpdateTimeRange,
   onToggleSectionLock,
   onSectionUpdate,
   onUnlockSection,
 }) {
-  const [newWebsite, setNewWebsite] = useState("");
-  const [editingIndex, setEditingIndex] = useState(null);
-  const [editingValue, setEditingValue] = useState("");
-  const [showLockOptions, setShowLockOptions] = useState(false);
   const [lockValue, setLockValue] = useState("");
   const [unlockAttempt, setUnlockAttempt] = useState("");
   const [wasSuccesfullyUnlocked, setWasSuccessfullyUnlocked] = useState(false);
@@ -42,27 +19,6 @@ export default function BlockedSitesSection({
     startTime: "",
     endTime: "",
   });
-
-  const handleAddWebsite = () => {
-    onAddWebsite(newWebsite);
-    setNewWebsite("");
-  };
-
-  const handleEditWebsite = (siteIndex, site) => {
-    setEditingIndex(siteIndex);
-    setEditingValue(site);
-  };
-
-  const handleSaveEdit = () => {
-    onEditWebsite(editingIndex, editingValue);
-    setEditingIndex(null);
-    setEditingValue("");
-  };
-
-  const handleCloseEditMenu = () => {
-    setEditingIndex(null);
-    setEditingValue("");
-  };
 
   const toggleLockOptions = () => {
     setShowLockOptions(!showLockOptions);
@@ -349,125 +305,25 @@ export default function BlockedSitesSection({
     onToggleSectionLock(index);
   };
 
-  return (
-    <div className="blocker-sites-section">
-      <h2 className="section-name" onClick={() => onToggleModal(index)}>
-        {title}
-      </h2>
-      {isModalOpen && (
-        <ul>
-          {sites.map((site, siteIndex) => (
-            <li key={siteIndex} className="site-item">
-              {editingIndex === siteIndex ? (
-                <div>
-                  <input
-                    type="text"
-                    value={editingValue}
-                    onChange={(e) => setEditingValue(e.target.value)}
-                  />
-                  <TfiSave className="icon" onClick={handleSaveEdit} />
-                  <FaRegWindowClose
-                    className="icon"
-                    onClick={handleCloseEditMenu}
-                  />
-                  <div
-                    className="icon"
-                    onClick={() => setEditingIndex(null)}
-                  ></div>
-                </div>
-              ) : (
-                <div>
-                  <p className="site-name">{site}</p>
-
-                  <FaPencilAlt
-                    className="icon"
-                    onClick={() => handleEditWebsite(siteIndex, site)}
-                  />
-
-                  <AiOutlineDelete
-                    className="icon"
-                    onClick={() => onDeleteWebsite(siteIndex)}
-                  />
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-      )}
-      {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content ">
-            <div className="add-edit-website">
-              <input
-                type="text"
-                value={newWebsite}
-                onChange={(e) => setNewWebsite(e.target.value)}
-                placeholder="Enter new website"
-              />
-              <div className="icon-button" onClick={handleAddWebsite}>
-                <VscDiffAdded className="icon" />
-              </div>
-            </div>
-
-            <div className="add-edit-section">
-              <input
-                type="text"
-                value={title} // Use title from props directly
-                onChange={(e) => onEditSectionTitle(e.target.value)}
-                placeholder="Enter Section Title"
-              />
-              <div className="icon-button-container">
-                <AiOutlineDelete
-                  className="icon"
-                  onClick={() => onDeleteSection(index)}
-                />
-                <FaRegWindowClose className="icon" onClick={onCloseModal} />
-                <div className="icon" onClick={toggleLockOptions}>
-                  {showLockOptions ? <FaLockOpen /> : <FaLock />}
-                </div>
-              </div>
-            </div>
-          </div>
-          {showLockOptions && (
-            <div className="lock-section">
-              <LockOptions
-                section ={section}
-                index = {index}
-                onLockMethodChange = {onLockMethodChange}
-                onUpdateTimeRange ={onUpdateTimeRange}
-                onToggleSectionLock = {onToggleSectionLock}
-                onSectionUpdate = {onSectionUpdate}
-                onUnlockSection ={onUnlockSection}
-                {renderLockMethodSelector()} 
-                {renderLockInputs()} {/* Render Lock Inputs */}
-                {renderLockToggle()} {/* Render Lock Toggle */}
-              />
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
+  LockOptions.propTypes = {
+    index: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    sites: PropTypes.array.isRequired,
+    section: PropTypes.object.isRequired,
+    isModalOpen: PropTypes.bool.isRequired,
+    onOpenModal: PropTypes.func.isRequired,
+    onCloseModal: PropTypes.func.isRequired,
+    onToggleModal: PropTypes.func.isRequired,
+    onAddWebsite: PropTypes.func.isRequired,
+    onEditWebsite: PropTypes.func.isRequired,
+    onEditSectionTitle: PropTypes.func.isRequired,
+    onDeleteSection: PropTypes.func.isRequired,
+    onDeleteWebsite: PropTypes.func.isRequired,
+    onUpdateTimeRange: PropTypes.func.isRequired,
+    onToggleSectionLock: PropTypes.func.isRequired,
+    onLockMethodChange: PropTypes.func.isRequired,
+    onLockSubmit: PropTypes.func.isRequired,
+    onUnlockSection: PropTypes.func.isRequired,
+    onSectionUpdate: PropTypes.func.isRequired,
+  };
 }
-
-BlockedSitesSection.propTypes = {
-  index: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  sites: PropTypes.array.isRequired,
-  section: PropTypes.object.isRequired,
-  isModalOpen: PropTypes.bool.isRequired,
-  onOpenModal: PropTypes.func.isRequired,
-  onCloseModal: PropTypes.func.isRequired,
-  onToggleModal: PropTypes.func.isRequired,
-  onAddWebsite: PropTypes.func.isRequired,
-  onEditWebsite: PropTypes.func.isRequired,
-  onEditSectionTitle: PropTypes.func.isRequired,
-  onDeleteSection: PropTypes.func.isRequired,
-  onDeleteWebsite: PropTypes.func.isRequired,
-  onUpdateTimeRange: PropTypes.func.isRequired,
-  onToggleSectionLock: PropTypes.func.isRequired,
-  onLockMethodChange: PropTypes.func.isRequired,
-  onLockSubmit: PropTypes.func.isRequired,
-  onUnlockSection: PropTypes.func.isRequired,
-  onSectionUpdate: PropTypes.func.isRequired,
-};
