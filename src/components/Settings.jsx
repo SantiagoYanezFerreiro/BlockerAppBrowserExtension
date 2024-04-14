40c22401aa39dc133f4511323842e88c50fd0dc6
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-04-14 23:54:22 +0200

    logic to display settings sections based on button clicks

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import "../Settings.css";
+
 export default function Settings() {
+  const [activeTab, setActiveTab] = useState("general");
   return (
-    <div>
-      <h1>Settings</h1>
+    <div className="settings-containter">
+      <div className="tabs">
+        <button
+          onClick={() => setActiveTab("general")}
+          className={activeTab === "general" ? "active" : ""}
+        >
+          General
+        </button>
+        <button
+          onClick={() => setActiveTab("blocking")}
+          className={activeTab === "blocking" ? "active" : ""}
+        >
+          Blocking Rules
+        </button>
+        <button
+          onClick={() => setActiveTab("security")}
+          className={activeTab === "security" ? "active" : ""}
+        >
+          Security
+        </button>
+        <button
+          onClick={() => setActiveTab("notifications")}
+          className={activeTab === "notifications" ? "active" : ""}
+        >
+          Notifications
+        </button>
+        <button
+          onClick={() => setActiveTab("statistics")}
+          className={activeTab === "statistics" ? "active" : ""}
+        >
+          Statistics
+        </button>
+      </div>
+      <div className="tab-content">
+        {activeTab === "general" && <GeneralSettings />}
+        {activeTab === "blocking" && <BlockingRules />}
+        {activeTab === "security" && <Security />}
+        {activeTab === "notifications" && <Notifications />}
+        {activeTab === "statistics" && <Statistics />}
+      </div>
     </div>
   );
+}
+
+function GeneralSettings() {
+  return <div> General Settings </div>;
+}
+
+function BlockingRules() {
+  return <div> Blocking Rules </div>;
+}
+
+function Security() {
+  return <div> Security </div>;
+}
+
+function Notifications() {
+  return <div> Notifications </div>;
+}
+
+function Statistics() {
+  return <div> Statistics</div>;
 }
