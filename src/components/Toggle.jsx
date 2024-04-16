f8d3ad7cd931f0e5f7f0b150c1111b180f548428
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-04-16 23:38:03 +0200

    corrected proptypes typo

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import PropTypes from "prop-types";
 import "./Toggle.css";
 
-export default function Toggle =({isEnabled, onToggle, label})=>{
-  return(
+export default function Toggle({ isEnabled, onToggle, label }) {
+  return (
     <div className="toggle-container">
-        {label && <span className="toggle-label">{label}:</span>}
-        <label className="toggle-switch">
-            <input
-                type="checkbox"
-                checked={isEnabled}
-                onChange={onToggle}
-            />
-            <span className="slider round"></span>
-        </label>
+      {label && <span className="toggle-label">{label}:</span>}
+      <label className="toggle-switch">
+        <input type="checkbox" checked={isEnabled} onChange={onToggle} />
+        <span className="slider round"></span>
+      </label>
     </div>
-  )  
+  );
+}
+
+Toggle.propTypes = {
+  isEnabled: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  label: PropTypes.string,
 };
