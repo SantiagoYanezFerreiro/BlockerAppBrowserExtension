454906a9d3e372ee23d3135f1941326fe1f1e525
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-04-27 23:02:09 +0200

    base chart for blocked stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -36,11 +36,9 @@ export default function Stats() {
   return (
     <div>
       <h1>Stats</h1>
-      <h2>Blocked Websites</h2>
+      <h2>Blocked Websites/Apps</h2>
       <BlockedStats data={blockedData} />
-      <h2>Blocked Applications</h2>
-      <h2>Websites Statistics</h2>
-      <h2>Application Statistics</h2>
+      <h2>Websites/Apps Usage Statistics</h2>
     </div>
   );
 }
