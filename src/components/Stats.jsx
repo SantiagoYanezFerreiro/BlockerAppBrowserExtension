ac3e82cef24eaf50db5cbc41608e096917fc093c
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-04-27 22:05:15 +0200

    main stats graph titles

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,6 +2,10 @@ export default function Stats() {
   return (
     <div>
       <h1>Stats</h1>
+      <h2>Blocked Websites</h2>
+      <h2>Blocked Applications</h2>
+      <h2>Websites Statistics</h2>
+      <h2>Application Statistics</h2>
     </div>
   );
 }
