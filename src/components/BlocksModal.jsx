c81796e110f093e676b5891874f345615d38c367
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-04-28 22:38:57 +0200

    structured Modal component using props from other components to add edit and delete websites

--- src/components/BlocksModal.jsx
+++ src/components/BlocksModal.jsx
@@ -8,12 +8,46 @@ export default function BlocksModal({
   editWebsitesInSection,
   deleteWebsiteFromSection,
 }) {
-  const [newWebsite, setNewWebsite] = React.useState("");
+  const [newWebsite, setNewWebsite] = useState("");
 
   const handleAddWebsite = () => {
     addWebsitesToSection(section.index, newWebsite);
     setNewWebsite("");
   };
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content"></div>
+      <span className="model-close" onClick={closeModal}>
+        &times;
+      </span>
+      <h2>Edit Sites for {section.title}</h2>
+      <div className="modal-setion">
+        <input
+          type="text"
+          placeholder="Enter Website URL.."
+          value={newWebsite}
+          onChange={(e) => setNewWebsite(e.target.value)}
+        />
+        <button onClick={handleAddWebsite}>Add</button>
+      </div>
+      <ul>
+        {section.sties.map((site, index) => {
+          <li key={index}>
+            {site}
+            <button
+              onClick={() => editWebsitesInSection(section.index, index, site)}
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => deleteWebsiteFromSection(section.index, index)}
+            ></button>
+          </li>;
+        })}
+      </ul>
+      <button onClick={closeModal}>Close</button>
+    </div>
+  );
 }
 
 BlocksModal.propTypes = {
