694d5f8391e3579011e7e0e54ccb2f1426f00709
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2024-04-28 16:45:49 +0200

    styled overview tab , wasn't aligned and font too small

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -4,23 +4,25 @@ import { IoLockClosedOutline } from "react-icons/io5";
 
 export default function Overview({ sections }) {
   return (
-    <div className="overview-container">
-      <h1>Active Blocks</h1>
-      {sections.map((section, index) => (
-        <div key={index} className="section-overview">
-          <h3>{section.title}</h3>
-          <h3>
-            {section.lockMethod === "password" ||
-            section.lockMethod === "randomText"
-              ? "No Breaks"
-              : "No Allowance"}
-          </h3>
-          <h3>{section.lockMethod}</h3>
-          <div className="lock-icon-container">
-            <IoLockClosedOutline />
+    <div className="overview-wrapper">
+      <div className="overview-container">
+        <h1>Active Blocks</h1>
+        {sections.map((section, index) => (
+          <div key={index} className="section-overview">
+            <h3 className="overview-section-title">{section.title}</h3>
+            <h3 className="overview-section-lock-message">
+              {section.lockMethod === "password" ||
+              section.lockMethod === "randomText"
+                ? "No Breaks"
+                : "No Allowance"}
+            </h3>
+            <h3 className="overview-section-lock-type">{section.lockMethod}</h3>
+            <div className="lock-icon-container">
+              <IoLockClosedOutline />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
